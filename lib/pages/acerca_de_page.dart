101314fee1d32676577a1827b084b920a7ea4027
Marc-Morillo <202010624@itla.edu.do>
2024-12-03 09:37:09 -0400

    Agregando los desarrolladores

--- lib/pages/acerca_de_page.dart
+++ lib/pages/acerca_de_page.dart
@@ -13,22 +13,26 @@ class AcercaDePage extends StatelessWidget {
               imagePath: 'assets/marcos.png',
               name: 'Marcos Jose Morillo Suarez',
               matricula: 'Matrícula: 2020-10624',
+              aporte: 'Desarrollo de la aplicacion',
             ),
-            // DeveloperInfo(
-            //   imagePath: 'assets/vago2.jpg',
-            //   name: 'Yesica De Los Santos',
-            //   matricula: 'Matrícula: 2022-0748',
-            // ),
-            // DeveloperInfo(
-            //   imagePath: 'assets/vago3.jpg',
-            //   name: 'Eduardo Pérez',
-            //   matricula: 'Matrícula: 2020-10473',
-            // ),
-            // DeveloperInfo(
-            //   imagePath: 'assets/vago1.jpg',
-            //   name: 'Vicmairy Adelina Charles Laureano',
-            //   matricula: 'Matrícula: 2022-0261',
-            // ),
+            DeveloperInfo(
+            imagePath: 'assets/vago2.jpg',
+            name: 'Yesica De Los Santos',
+            matricula: 'Matrícula: 2022-0748',
+            aporte: 'Absolutamente nada',
+            ),
+            DeveloperInfo(
+            imagePath: 'assets/vago3.jpg',
+            name: 'Eduardo Pérez',
+            matricula: 'Matrícula: 2020-10473',
+            aporte: 'Absolutamente nada',
+            ),
+            DeveloperInfo(
+            imagePath: 'assets/vago1.jpg',
+            name: 'Vicmairy Adelina Charles Laureano',
+            matricula: 'Matrícula: 2022-0261',
+            aporte: 'Absolutamente nada',
+            ),
           ],
         ),
       ),
@@ -40,11 +44,13 @@ class DeveloperInfo extends StatelessWidget {
   final String imagePath;
   final String name;
   final String matricula;
+  final String aporte;
 
   const DeveloperInfo({
     required this.imagePath,
     required this.name,
     required this.matricula,
+    required this.aporte,
   });
 
   @override
